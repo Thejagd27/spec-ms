fd423d09f51dc867904f51b3ff2ec8324f7d3e32
dhanush-2397 <dhanush.ks@tibilsolutions.com>
2023-03-25 13:28:38 +0530

    changes in controller

--- src/specifications/controller/specification.controller.ts
+++ src/specifications/controller/specification.controller.ts
@@ -1,4 +1,3 @@
-import { PipelineGenericService } from './../service/pipeline-generic/piepline-generic.service';
 import {EventService} from './../service/event/event.service';
 import {DimensionService} from './../service/dimension/dimension.service';
 import {Body, Controller, Get, Param, Post, Res, Query} from '@nestjs/common';
@@ -13,19 +12,17 @@ import {
     specEventDTO,
     s3DTO, GetGrammar
 } from '../dto/specData.dto';
-import {TransformerService} from '../service/transformer/transformer.service';
 import {DatasetService} from '../service/dataset/dataset.service';
 import {ScheduleService} from '../service/schedule/schedule.service';
 import {ApiTags} from '@nestjs/swagger';
 import {S3Service} from '../service/s3/s3.service';
-import { PipelineService } from '../service/pipeline-old/pipeline.service';
 import {Grammar} from '../service/grammar/grammar.service';
 
 @ApiTags('spec-ms')
 @Controller('/spec')
 export class SpecificationController {
-    constructor(private dimensionService: DimensionService, private EventService: EventService, private transformerservice: TransformerService, private datasetService: DatasetService,
-                private pipelineService: PipelineService, private scheduleService: ScheduleService, private s3service: S3Service,private pipelineGeneric:PipelineGenericService, private grammar: Grammar) {
+    constructor(private dimensionService: DimensionService, private EventService: EventService, private datasetService: DatasetService,
+                 private scheduleService: ScheduleService, private s3service: S3Service, private grammar: Grammar) {
     }
 
     @Get('/hello')
@@ -42,8 +39,8 @@ export class SpecificationController {
             }
             else {
                 response.status(200).send({
-                    "message": result.message,
-                    "dimension_name": result.program,
+                    "message": result?.message,
+                    "dimension_name": result?.program,
                 });
             }
         } catch (error) {
@@ -88,50 +85,10 @@ export class SpecificationController {
         }
     }
 
-    @Post('/transformer')
-    async createTransformer(@Body() transformerDTO: specTrasformer, @Res()response: Response) {
-        try {
-            const result: any = await this.transformerservice.createTransformer(transformerDTO)
-            if (result.code == 400) {
-                response.status(400).send({"message": result.error});
-            }
-            else {
-                response.status(200).send({"message": result.message, "response": result.response});
-            }
-        } catch (error) {
-            console.error("create.Transformer impl :", error)
-            throw new Error(error);
-        }
-    }
+   
 
-    @Post('/pipeline-old')
-    async createPipeline(@Body() pipelineDto: pipelineDto, @Res()response: Response) {
-        try {
-            const result: Result = await this.pipelineService.createSpecPipeline(pipelineDto)
-            if (result?.code == 400) {
-                response.status(400).send({"message": result.error});
-            }
-            else {
-                response.status(200).send({"message": result.message});
-            }
-        } catch (error) {
-            console.error("create.Pipeline impl :", error)
-        }
-    }
-    @Post('/pipeline')
-    async createPipelineSpec(@Body() pipelineDto: pipelineDto, @Res()response: Response) {
-        try {
-            const result: Result = await this.pipelineGeneric.createSpecPipeline(pipelineDto)
-            if (result?.code == 400) {
-                response.status(400).send({"message": result.error});
-            }
-            else {
-                response.status(200).send({"message": result.message});
-            }
-        } catch (error) {
-            console.error("create.Pipeline impl :", error)
-        }
-    }
+    
+   
 
     @Post('/schedule')
     async schedulePipeline(@Body() scheduleDto: scheduleDto, @Res()response: Response) {
@@ -165,20 +122,20 @@ export class SpecificationController {
     }
 
     
-    @Post('/pipeline')
-    async createSpecPipeline(@Body() pipelineDto: pipelineDto, @Res()response: Response) {
-        try {
-            const result: any = await this.pipelineService.createSpecPipeline(pipelineDto);
-            if (result?.code == 400) {
-                response.status(400).send({"message": result.error});
-            }
-            else {
-                response.status(200).send({"message": result.message});
-            }
-        } catch (error) {
-            console.error('createSpecPipelineNew: ', error);
-        }
-    }
+    // @Post('/pipeline')
+    // async createSpecPipeline(@Body() pipelineDto: pipelineDto, @Res()response: Response) {
+    //     try {
+    //         const result: any = await this.pipelineService.createSpecPipeline(pipelineDto);
+    //         if (result?.code == 400) {
+    //             response.status(400).send({"message": result.error});
+    //         }
+    //         else {
+    //             response.status(200).send({"message": result.message});
+    //         }
+    //     } catch (error) {
+    //         console.error('createSpecPipelineNew: ', error);
+    //     }
+    // }
 
     @Get('/grammar')
     async getGrammar(@Query()getGrammar: GetGrammar, @Res() response: Response) {
